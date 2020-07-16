53d0bd31f70739225db02fa348e9bc3b23f59d2e
donRumata <donrumata03@gmail.com>
2020-07-17 02:11:16 +0300

    add stroke color search function signatures

--- optimization/stroke_color_optimizing.h
+++ optimization/stroke_color_optimizing.h
@@ -8,4 +8,9 @@
 #include "utils/stroke.h"
 #include "error_computing.h"
 
-colored_stroke find_stroke_color();
+color find_stroke_color(const stroke& colorless_stroke, const Image& image);
+// ^^^ TODO: implement it somehow! (local optimization... after using dummy function)
+
+
+color find_stroke_color_by_ariphmetic_mean(const stroke& colorless_stroke, const Image& image);
+// ^^^ TODO
