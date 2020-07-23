71c305e1618f702f8423ef698f1bf4ffaf434e47
donRumata <donrumata03@gmail.com>
2020-07-23 21:00:52 +0300

    start moving to computer

--- optimization/error_computing.cpp
+++ optimization/error_computing.cpp
@@ -6,31 +6,43 @@
 
 #include <execution>
 
-double image_mse (const Image &image1, const Image &image2)
+double image_mse (const Image &image1, const Image &image2, bool parallel)
 {
 	assert(image1.size == image2.size());
 	size_t w = image1.cols, h = image1.rows;
 	auto area = double(w * h);
 
 	double diff_sum = 0;
-	for (size_t x = 0; x < w; ++x) {
-		for (size_t y = 0; y < h; ++y) {
-			const auto& first_pixel = image1.at<cv::Vec3d>(y, x);
-			const auto& second_pixel = image2.at<cv::Vec3d>(y, x);
 
-			/*
-			double red_diff = square( first_pixel[0] - second_pixel[0] );
-			double green_diff = square( first_pixel[1] - second_pixel[1] );
-			double blue_diff = square( first_pixel[2] - second_pixel[2] );
+	if (not parallel) {
+		for (size_t x = 0; x < w; ++x) {
+			for (size_t y = 0; y < h; ++y) {
+				const auto &first_pixel = image1.at<cv::Vec3d>(y, x);
+				const auto &second_pixel = image2.at<cv::Vec3d>(y, x);
 
-			diff_sum += red_diff + green_diff + blue_diff;
-			*/
-			for (size_t dim_index = 0; dim_index < 3; ++dim_index) {
-				diff_sum += square(second_pixel[dim_index] - first_pixel[dim_index]);
+				// TODO: parallelize for loop!
+
+				/*
+				double red_diff = square( first_pixel[0] - second_pixel[0] );
+				double green_diff = square( first_pixel[1] - second_pixel[1] );
+				double blue_diff = square( first_pixel[2] - second_pixel[2] );
+
+				diff_sum += red_diff + green_diff + blue_diff;
+				*/
+				for (size_t dim_index = 0; dim_index < 3; ++dim_index) {
+					diff_sum += square(second_pixel[dim_index] - first_pixel[dim_index]);
+				}
 			}
 		}
 	}
-
+	else { // Parallel:
+		image1.forEach<Pixel>([&](Pixel& first_pixel_value, const int position[]){
+			// TODO: x, y or y, x? => define first and second pixels
+			for (size_t dim_index = 0; dim_index < 3; ++dim_index) {
+				diff_sum += square(second_pixel[dim_index] - first_pixel[dim_index]);
+			}
+		});
+	}
 
 	return diff_sum / area;
 }
--- optimization/error_computing.h
+++ optimization/error_computing.h
@@ -8,7 +8,7 @@
 #include "utils/stroke.h"
 
 
-double image_mse(const Image& image1, const Image& image2);
+double image_mse(const Image& image1, const Image& image2, bool parallel = false); // TODO: make parallel
 double stroke_mse(const Image& image, const colored_stroke& stroke, size_t step_number = 100, bool parallel = false);
 double stroke_mse (const Image &image, const std::vector<stroke::point> &stroke_points, const color &stroke_color, bool parallel = false);
 
