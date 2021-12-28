b5768097f6fba85e94c75684d9a2b3c1e47d47e2
donRumata03 <donrumata03@gmail.com>
2021-12-28 20:06:07 +0300

    Temporarily remove template

--- rasterization/layer_tracking/rasterized_paining.h
+++ rasterization/layer_tracking/rasterized_paining.h
@@ -6,8 +6,8 @@
 #include "rasterization/layer_tracking/pixel_managers/treemap_pixel_layer_tracker.h"
 
 
-template<PixelLayerTracker Tracker>
-struct RasterizedPainting {
+template<typename Tracker>
+struct RasterizedPainting /* requires PixelLayerTracker<Tracker> */ {
   using PixelSet = std::vector<ColoredPosition>;
 
 
