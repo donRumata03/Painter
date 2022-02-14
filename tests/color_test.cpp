a36e7c160b1914579780792a66cc48c3a67cc9ee
donRumata <donrumata03@gmail.com>
2022-02-14 14:32:55 +0300

    refactor: Rename color conversion tests

--- tests/color_test.cpp
+++ tests/color_test.cpp
@@ -3,7 +3,7 @@
 #include "data/color.h"
 
 
-TEST(ConvertTest, ColorToFloatingPoint) {
+TEST(Color, ToAndFromFloatingPoint) {
   ByteColor color = {255, 100, 1};
   auto float_color = to_floating_point<double, uint8_t>(color);
   auto byte_color = from_floating_point<uint8_t, double>(float_color);
